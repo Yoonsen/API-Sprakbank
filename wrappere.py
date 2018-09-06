94aded39fcf13dd6309bc3b1c8085221dc357b2c
Lars G Johnsen <yoonsen@gmail.com>
2018-09-06 12:44:47 +0200

    Create wrappere.py

---
+++ wrappere.py
@@ -0,0 +1,12 @@
+def get_freq(urn, top=50, cutoff=3):
+    r = requests.get("https://api.nb.no/ngram/urnfreq", json={'urn':urn, 'top':top, 'cutoff':cutoff})
+    return Counter(dict(r.json()))
+
+def get_papers(top=5, cutoff=5, navn='%', yearfrom=1800, yearto=2020, samplesize=10):
+    r = requests.get("https://api.nb.no/ngram/avisfreq", json={'navn':navn, 'top':top, 'cutoff':cutoff,
+                                                              'yearfrom':yearfrom, 'yearto':yearto,'samplesize':samplesize})
+    return r.json()
+
+def collocation(word, yearfrom=2010, yearto=2018, corpus='avis'):
+    data =  requests.get("https://api.nb.no/ngram/collocation", params={'word':word,'corpus':corpus, 'yearfrom':yearfrom, 'yearto':yearto}).json()
+    return pd.DataFrame.from_dict(data['freq'], orient='index')
